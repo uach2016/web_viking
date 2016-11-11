0b624cb7523ab42bc6bb0c90db8d5240feaf03cf
uach2016 <uacaminoherera@gmail.com>
2016-11-11 10:11:28 +0000

    controller and view

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+
+  config.api_key = "f387f16a644311db8e21332a6dd7a759"
+  config.secret = "e8dcdaac25090392bde55f7d46ca515d"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
